ee625875b78d4ae33b559e5887ded096a03c657b
Samiser <sheney10@gmail.com>
2022-12-04 02:03:05 +0000

    day 3, now with partials

--- day-3/main.py
+++ day-3/main.py
@@ -1,13 +1,15 @@
+from functools import partial
+
 priority = lambda letter : ord(letter) - 96 if ord(letter) >= 97 else ord(letter) - 38
-priority_bag = lambda items : list(map(priority, items))
+bag = lambda type, items : type(map(priority, items))
 
 def part1(items):
-    pocket_pairs = map(lambda bag : [bag[:len(bag) // 2], bag[len(bag) // 2:]], map(priority_bag, items))
+    pocket_pairs = map(lambda bag : [bag[:len(bag) // 2], bag[len(bag) // 2:]], map(partial(bag, list), items))
     print(sum(map(lambda pair : set(pair[0]).intersection(set(pair[1])).pop(), pocket_pairs)))
 
 def part2(items):
-    groups = zip(*[iter(map(priority_bag, items))] * 3)
-    print(sum(map(lambda group : set(group[0]).intersection(group[1], group[2]).pop(), groups)))
+    groups = zip(*[iter(map(partial(bag, set), items))] * 3)
+    print(sum(map(lambda group : set.intersection(*group).pop(), groups)))
 
 if __name__ == '__main__':
     with open("input.txt") as f:
